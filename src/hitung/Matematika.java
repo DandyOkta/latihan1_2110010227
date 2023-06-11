bd58923427cf2f612d8b0c8660a309059affee0c
DandyOkta <oktadandy13@gmail.com>
2023-06-11 23:28:30 +0800

    Membuat Contructor dan setPenjumlahan

--- src/hitung/Matematika.java
+++ src/hitung/Matematika.java
@@ -10,5 +10,14 @@ package hitung;
  * @author acer
  */
 public class Matematika {
+    private double bil1, bil2;
+
+    public Matematika(double bil1, double bil2) {
+        this.bil1 = bil1;
+        this.bil2 = bil2;
+    }
     
+    public double setPenjumlahan(){
+        return bil1 + bil2;
+    }
 }
